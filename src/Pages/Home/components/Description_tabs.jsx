8680d3d92b512ef668948c01435d89e28e6cbbf2
Gano603 <ganogaming603@example.com>
2023-12-06 22:11:26 +0500

    Mobile Screen Changes

--- src/Pages/Home/components/Description_tabs.jsx
+++ src/Pages/Home/components/Description_tabs.jsx
@@ -6,7 +6,7 @@ const Description_tabs = ({ description_title, description_data, selected, cond,
   return (
     <>
       <div onClick={() => selected.includes(iter) ? setselected(selected.filter((index) => index !== iter)) : setselected([...selected, iter])} className={`${cond ? "h-16 mt-4" : "h-12"} transition-all duration-300 py-2 px-4 w-full bg-black bg-opacity-60 border-b-[1px] first:mt-0 last:border-none border-opacity-30 flex items-center shadow-lg border-black justify-between cursor-pointer`}>
-        <span className='text-white font-bold text-md'>{description_title}</span>
+        <span className='text-white font-bold text-sm sm:text-base'>{description_title}</span>
         <IoIosArrowDown className={`scale-150 text-white mr-2 sm:mr-6 transition-all duration-300 ${cond? "rotate-180":""}`}/>
       </div>
         <p
